e74f304394d7098060ef5c521e89cc14f4a511a5
= <=>
2024-04-24 15:25:33 -0600

    Is working now with bash script to be added from pi

--- socket_server.py
+++ socket_server.py
@@ -7,7 +7,7 @@ import numpy as np
 import struct
 
 
-HOST = '127.0.0.1'
+HOST = '10.0.0.36'
 PORT = 8083
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,23 +18,29 @@ print('Socket bind complete')
 s.listen(10)
 print('Socket now listening')
 
-now = datetime.now()
-fileName = 'vid' + now.strftime('%m%d%H:%M')
 
-file = open(fileName, 'w')
-
-
-data = b''
-payload_size = struct.calcsize("L")
-
-while True:
+while (True):
     conn, addr = s.accept()
-    data = conn.recv(4096)
-    
-    while(data):
-        file.write(data)
-        data = conn.recv(4096)
 
-    file.close()
-    conn.close()
+    now = datetime.now()
+    fileName = 'invid\\vid' + now.strftime('%m%d%H%M%S') + '.avi'
+    print(fileName)
+
+    file = open(fileName, 'wb')
+    print('file open')
+    
+    if(file):
+        data = b''
+        payload_size = struct.calcsize("L")
+
+        data = conn.recv(4096)
+        print('receiving')
+        
+        while(data):
+            file.write(data)
+            data = conn.recv(4096)
+
+        print(f'finished writing {fileName}')
+        file.close()
+        conn.close()
      
